c856975fc65abc6cd130a008742f3a3b50757be7
AungHtetPi <141935338+AungHtetPi@users.noreply.github.com>
2024-01-29 00:37:14 +0630

    surpriseGift

---
+++ Propose/script.js
@@ -0,0 +1,23 @@
+const warpper = document.querySelector(".warapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "Did you say yes? Ya hoo...";
+  gif.src =
+    "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
+});
+
+noBtn.addEventListener("click", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.height;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
